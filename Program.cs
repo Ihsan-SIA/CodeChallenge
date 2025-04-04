6b00d25def73085cc667beffb7cc0378303b3a7e
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-04-04 02:32:42 +0100

    Game to sum all number inputs or enter ok to end sum

--- Program.cs
+++ Program.cs
@@ -4,16 +4,3 @@ using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
-
-namespace CodeChallenge
-{
-    
-    internal class Program
-    {
-        static void Main()
-        {
-            
-        }
-    }
-    
-}
