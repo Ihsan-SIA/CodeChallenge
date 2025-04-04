74fa510c4fb7915c059616251cb7659f88252727
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-04-04 02:32:42 +0100

    Design of a Stack Program Stack Design

---
+++ AddNumGame.cs
@@ -0,0 +1,37 @@
+﻿class AddNumGame
+{
+        public static void EnterOk()
+    {
+        Console.WriteLine("Enter a number to add or ok to exit");
+        string userInput = Console.ReadLine()!;
+        double sum = 0;
+        double number = 0;
+        bool inputdoub = double.TryParse(userInput, out number);
+        bool checkInput = false;
+
+        do
+        {
+            switch (inputdoub)
+            {
+                case true:
+                    sum += number;
+                    Console.WriteLine("Enter a number to add or ok to exit");
+                    userInput = Console.ReadLine()!;
+                    inputdoub = double.TryParse(userInput, out number); break;
+                case false:
+                    switch (userInput.ToLower())
+                    {
+                        case "ok":
+                            checkInput = true; break;
+                        default:
+                            Console.Write("You should've followed instructions, now enter ok or a number lil bro: ");
+                            userInput = Console.ReadLine()!;
+                            inputdoub = double.TryParse(userInput, out number); break;
+                    }
+                    break;
+            }
+        }
+        while (!checkInput);
+        Console.WriteLine("The total sum of numbers entered is {0}", sum);
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,30 @@
+﻿public class Stack
+{
+    private List<object> _stackObjects = new List<object>();
+    public void StackMenu()
+    {
+
+    }
+    public void Push(object obj)
+    {
+        if (obj is null)
+        {
+            throw new InvalidCastException("Sorry the stack does not accept null values for now, maybe some other time.");
+        }
+        _stackObjects.Add(obj);
+    }
+    public object Pop()
+    {
+        if (!_stackObjects.Any())
+        {
+            throw new InvalidOperationException("Sorry stack is empty for now. Push an object to fill it, then come back.");
+        }
+        object anObject = _stackObjects.Last();
+        _stackObjects.Remove(_stackObjects.Last());
+        return anObject;
+    }
+    public void Clear()
+    {
+        _stackObjects.Clear();
+    }
+}
